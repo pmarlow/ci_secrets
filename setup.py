1b0f5c5658a38cfe872264e929e4c29300b27562
Phillip Marlow <phillip@marlow1.com>
2019-03-05 16:49:46 -0500

    Add missing gitpython dependency.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name='ci_secrets',
       author='Phillip Marlow',
       author_email='phillip@marlow1.com',
       packages=['ci_secrets'],
-	  install_requires=["detect_secrets"],
+	  install_requires=["gitpython","detect_secrets"],
 	  entry_points={
 		'console_scripts': [
             'ci_secrets = ci_secrets:main'
