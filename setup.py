f65cbab788da4a3b5c8a3f7fbc504c156c843637
Phillip Marlow <phillip@marlow1.com>
2019-08-04 18:22:51 -0400

    Fix indentation in setup.py.

--- setup.py
+++ setup.py
@@ -5,17 +5,17 @@ setup(name='ci_secrets',
       description='A secrets scanner for CI/CD.',
       author='Phillip Marlow',
       author_email='phillip@marlow1.com',
-	  url="https://github.com/pmarlow/ci_secrets",
+      url="https://github.com/pmarlow/ci_secrets",
       packages=['ci_secrets'],
-	  install_requires=["gitpython","detect_secrets"],
-	  entry_points={
-		'console_scripts': [
+      install_requires=["gitpython","detect_secrets"],
+      entry_points={
+        'console_scripts': [
             'ci_secrets = ci_secrets:main'
         ]
-	  },
-	  classifiers=[
-		"License :: OSI Approved :: Apache Software License",
-		"Programming Language :: Python :: 3",
-		"Topic :: Security",
-		"Topic :: Software Development"
-	  ])
+      },
+      classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Topic :: Security",
+        "Topic :: Software Development"
+      ])
